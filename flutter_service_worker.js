0ea140ad37bd8a5629aebd64c17f53ea1baa60e8
Swapnil <swapnilgupta.iiith@gmail.com>
2025-08-15 22:38:28 +0530

    Updating code to adopt to mobile screens

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "a9f103c10081431e50501ff2ac78dae3",
+"flutter_bootstrap.js": "0ca30b947bac1bd9748cb9fc08a8a6fe",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "453608b2c86d52002289c47a17c27078",
 "/": "453608b2c86d52002289c47a17c27078",
-"main.dart.js": "8bde56647915a5bc627108697046ae9e",
+"main.dart.js": "d2004d6b19a5d94ece0eaff1da1fccdd",
 "manifest.json": "7842fd14116096b27c185d2a1c01e790",
 "version.json": "20b860c7ab6f93bc2b5fda2b9e9137a2"};
 // The application shell files that are downloaded before a service worker can
